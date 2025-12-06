32653bdc9e6cf4ff522cecd45a835a2bd13c52f6
Mitchell Olsen <olsenmd@my.lanecc.edu>
2025-12-06 01:13:28 -0800

    REST Files Added

---
+++ BitsCore/BitsREST/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BitsEFClasses;
+using BitsEFClasses.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BitsContext>(options =>
+{
+    options.UseMySql(
+        ConfigDB.GetMySqlConnectionString(),
+        new MySqlServerVersion(new Version(8, 0, 0)));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
